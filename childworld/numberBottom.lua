cbe373f50cba3ef7fa6a4021758f4d209f2897de
Lixiaxian <835273072@qq.com>
2022-08-30 14:15:15 +0800

    update

--- childworld/numberBottom.lua
+++ childworld/numberBottom.lua
@@ -1,25 +1,41 @@
-local html = [[
-    <div style="margin:200px;margin-top:0;">
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/1.png);" onclick="clickNumber" param="1"></span>
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/2.png);" onclick="clickNumber" param="2"></span>
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/3.png);" onclick="clickNumber" param="3"></span>
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/4.png);" onclick="clickNumber" param="4"></span>
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/5.png);" onclick="clickNumber" param="5"></span>
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/6.png);" onclick="clickNumber" param="6"></span>
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/7.png);" onclick="clickNumber" param="7"></span>
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/8.png);" onclick="clickNumber" param="8"></span>
-        <span style="margin-left:10;width: 159;height: 238;background: url(images/number/9.png);" onclick="clickNumber" param="9"></span>
-    </div>
-]]
+local cardSize = {
+    width = 159,
+    height = 238
+}
+local numCards = {}
+for i = 1, 9 do
+    numCards[i] = {
+        index = i,
+        bottom = 0,
+        left = 10,
+        width = cardSize.width,
+        height = cardSize.height
+    }
+end
 
+function getNumCards()
+    return numCards
+end
 
--- (159 + 10)* 9 = 1521,1521 + 200 * 2 = 1921 (如修改尺寸，需要改这个)
-local numbersWnd = window(html, "_ctb", 0, -20, 1921, 238)
+function getStyle(index)
+    local card = numCards[index]
+    local styleStr = string.format(
+        "margin-left:%s;margin-bottom:%s;width:%s;height:%s;background: url(images/number/%s.png)", card.left,
+        card.bottom, card.width, card.height, card.index)
+    return styleStr
+end
+
+local numbersWnd = window([[ 
+<div style="margin:200px;margin-top:0;">
+<pe:repeat DataSource="<%=getNumCards()%>">
+    <pe:repeatitem style="float:left;">
+        <div name='<%=Eval("name")%>' style='<%=getStyle(Eval("index"))%>' onclick="OnClickTest2" value='<%=Eval("name")%>'></div> 
+    </pe:repeatitem>
+</pe:repeat>
+</div>
+]], "_ctb", 0, -20, 1921, 238)
 numbersWnd:SetDesignResolution(1921, 238)
 
-local clickTime = 1 -- 记录第几次点击
-
-function clickNumber(i, mcmlNode)
-    local num = tonumber(mcmlNode:GetAttribute("param"))
-    tip(num)
+function OnClickTest2(name, mcmlNode)
+    tip(name)
 end
